71be9840679b92d3be81f0e01306f0c22b362b33
Sakibur Rahman Sabbir <106433632+gmsakibursabbir@users.noreply.github.com>
2023-10-17 21:20:48 +0600

    update

--- app.js
+++ app.js
@@ -45,13 +45,3 @@ console.log(newMap);
 
 
 
-const subjects = ["Maths", "Science", "Polity", "History"];
-let i = 0;
-let len = subjects.length;
-let gfg = "";
-for (; i < len;) {
-    gfg += subjects[i + ''];
-    //can be increased inside loop
-    i++;
-}
-console.log(gfg)
